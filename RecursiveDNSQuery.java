eb7b5b3c326941ace650301d9107d6efd14c641a
Md. Rahul Islam Joy <81201194+rahul-joy@users.noreply.github.com>
2023-10-25 13:27:33 +0600

    Add files via upload

---
+++ RecursiveDNSQuery.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rahul;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class rahul2{
+    private String domain;
+    private String ipAddress;
+    
+    public rahul2(String domain, String ipAddress){
+        this.domain=domain;
+        this.ipAddress=ipAddress;
+    }
+    
+    public String getDomain(){
+        return domain;
+    }
+    
+    public String getIpAddress(){
+        return ipAddress;
+    }
+}
+
+/**
+ *
+ * @author gub-cse
+ */
+class IterativeDNSQuery {
+    private List<rahul2> rahul;
+    
+    public IterativeDNSQuery(){
+        rahul = new ArrayList<>();
+        rahul.add(new rahul2("example.com", "93.184.216.34"));
+        rahul.add(new rahul2("example.org", "2606:2800:220:1:248:1893:25c8:1946"));
+        
+    }
+    public String findIpAddress(String domain) {
+        for (rahul2 record : rahul) {
+            if (record.getDomain().equals(domain)){
+                return record.getIpAddress();
+            }
+        }
+        return "not found";
+        
+    }
+    public static void main(String[] args) {
+    IterativeDNSQuery query = new IterativeDNSQuery();
+    System.out.println("example.com: " + query.findIpAddress("example.com"));
+    System.out.println("example.org: " + query.findIpAddress("example.org"));
+    }
+}
+
+public class RecursiveDNSQuery{
+    private rahul2[] rahul;
+    
+    public RecursiveDNSQuery(){
+        rahul=new rahul2[2];
+        rahul[0]=new rahul2("example.com", "93.184.216.34");
+        rahul[1]=new rahul2("example.org", "2606:2800:220:1:248:1893:25c8:1946");
+        
+    }
+    public String findIpAddress(String domain, int index){
+        if (index >= rahul.length) {
+            return "Not found";
+        }
+        if (rahul[index].getDomain().equals(domain)){
+            return rahul[index].getIpAddress();
+        }
+        return findIpAddress(domain, index + 1);
+    }
+    public static void main(String[] args) {
+    RecursiveDNSQuery query = new RecursiveDNSQuery();
+    System.out.println("example.com: " + query.findIpAddress("example.com",0));
+    System.out.println("example.org: " + query.findIpAddress("example.org",0));
+    }
+}
---
+++ rahul.java
@@ -0,0 +1,23 @@
+
+/**
+ *
+ * @author gub-cse
+ */
+public class rahul{
+    private String domain;
+    private String ipAddress;
+    
+    public rahul(String domain, String ipAddress){
+        this.domain=domain;
+        this.ipAddress=ipAddress;
+    }
+    
+    public String getDomain(){
+        return domain;
+    }
+    
+    public String getIpAddress(){
+        return ipAddress;
+    }
+}
+
